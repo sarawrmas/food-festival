8ee08012ead68cab5d716aad48607f6aec72b07a
Sara <sara.m.adamski@gmail.com>
2021-07-30 13:49:17 -0500

    modularize code

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,53 @@
-const path = require('path');
-const webpack = require('webpack');
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   // root of bundle and beginning of dependency graph
-  entry: './assets/js/script.js',
-  // output bundled code from entry and place into 'dist/main.bundle.js'
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
+  // output bundled code from entry and place into "dist/main.bundle.js"
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name (file) {
+                return "[path][name].[ext]"
+              },
+              publicPath: function(url) {
+                return url.replace("../", "/assets/")
+              }
+            }
+          },
+          {
+            loader: "image-webpack-loader"
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      // report outputs to an HTML file in the dist folder
+      analyzerMode: "static"
     })
   ],
   // bundle code using development mode
-  mode: 'development'
+  mode: "development"
 };
