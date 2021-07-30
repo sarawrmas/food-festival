b17662af4b260c4c714bad139b97775381dd87f7
Sara <sara.m.adamski@gmail.com>
2021-07-30 14:30:08 -0500

    add service worker

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const APP_PREFIX = 'FoodFest-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./events.html",
+  "./tickets.html",
+  "./schedule.html",
+  "./assets/css/style.css",
+  "./assets/css/bootstrap.css",
+  "./assets/css/tickets.css",
+  "./dist/app.bundle.js",
+  "./dist/events.bundle.js",
+  "./dist/tickets.bundle.js",
+  "./dist/schedule.bundle.js"
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      if (request) {
+        return request;
+      } else {
+        return fetch(e.request)
+      }
+    })
+  )
+})
