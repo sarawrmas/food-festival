7c9454bfa6277cf3a5d0aedf3fafb0c985dfb9c3
Sara <sara.m.adamski@gmail.com>
2021-09-27 14:11:52 -0500

    polish content, remove lorem ipsum, update readme

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 Visit the application at https://sarawrmas.github.io/food-festival/.
 
 ## Description
-Food truck festival, available as PWA, available offline
+Food festival is your guide to all things delicious. Join us for a weekend of events at this food truck jamboree with three themed events per day. This is a Progressive Web Application that users can install on their device and access without internet so they can check events at the festival and never miss a slot.
 
 ## Technologies
 * HTML
@@ -23,9 +23,20 @@ Food truck festival, available as PWA, available offline
 * Webpack
 
 ## Usage
+The home page presents with an interactive image carousel describing the event and prompting the user to see more of the website.
+
+Under the carousel, you can read testimonials, our mission statement, and see our rating.
+
+The footer contains links to our social media pages. Follow us on facebook, twitter, google, instagram, and pinterest.
+
+The navigation contains a link to the weekend's schedule, which gives a detailed overview of the events for each day with a short description.
+
+The tickets link in the navigation shows a table of ticket prices and allows you to purchase your tickets. Select the "Purchase Tickets" button and modal will pop up. You can select how many days you'd like to attend, how many passes you want, and an email address. After hitting purchase, you will see a notification that an email will be sent to you with a complete order form.
+
+At any time, you can click either "Home" or the title "Food Festival" in the navigation menu to return home.
 
 ## Preview
-![screenshot](assets/images/screenshot.png)
+![screenshot](assets/img/screenshot.png)
 
 ## Questions
 Have questions about this project?
--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -27,57 +27,57 @@ $(document).ready( function() {
   {
       title: 'Vegan Day',
       start: new Date(y, m+1, 20),
-      description: `Vegans unite! At this event, we'll be celebrating with vegan food from around the globe. With so many delicious vegan cuisine options, it's hard to pick one. What are you hungry for? Let us share with you!
-      Veganism is a lifestyle choice—we're passionate about creating healthy and delicious food products without harming the environment, so please spread the word about our festival and about our work!`,
+      description: `Vegans unite! At this event, we'll be celebrating with vegan food from around the globe. With so many delicious vegan cuisine options, it's hard to pick one. 
+      We're passionate about creating healthy and delicious food products without harming the environment, so please spread the word about our festival and about our work!`,
       image: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
   },
   {
       title: 'Harvest Festival',
       start: new Date(y, m+1, 20),
+      description: `Farmers rejoice! We will be celebrating our agricultural workers with a tasting menu revolved around fresh produce.
+      Get a taste of everything that's in season during this farm-to-table extravaganza!`,
       image: "https://images.unsplash.com/photo-1474440692490-2e83ae13ba29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
   },
   {
       title: 'Bacon Day',
       start: new Date(y, m+1, 20),
-      description: `Today's all about bacon! We'll be serving up the best tasting bacon around town! Let's all get excited about bacon together!
-      As an appetizer, grab a side of our delicious BBQ pulled pork and pick up a delicious side of our homemade slaw and sauce! There will be bacon everywhere!!`,
+      description: `Today's all about bacon! We'll be serving up the best tasting bacon around town. If you can dream it, we can bacon it. Get ready for bacon galore!`,
       image: "https://images.unsplash.com/photo-1528607929212-2636ec44253e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
   
   },
   {
       title: 'Fry-day',
-      description: `You bring it, we fry it. Everything tastes better fried! Whether it's your favorite sponge cake or a refreshing afternoon snack,
-      prepare to shock your tastebuds.`,
+      description: `You bring it, we fry it. Whether it's your favorite sponge cake or a refreshing afternoon snack, prepare to shock your tastebuds.`,
       start: new Date(y, m+1, 21),
       image: "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
   },
   {
       title: 'Pizza Party!',
-      description: `This event is devoted to all those pizza lovers out there. Come enjoy fresh from the oven pizza and learn tips on making pizza at home from the pros!`,
+      description: `This event is devoted to all those pizza lovers out there. Come enjoy fresh fire brick oven pizza made from scratch and learn tips on making pizza at home from the pros!`,
       start: new Date(y, m+1, 22),
       image: "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
   },
   {
       title: 'Pasta Bonanza',
-      description: `All things pasta.`,
+      description: `All things pasta. Rotini, rigatoni, cannelloni, tortellini, farfalle, paccheri, fettucine, gnocchi, and more!`,
       start: new Date(y, m+1, 22),
       image: "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
   },
   {
-      title: 'Fruity Foods!',
-      description: `We will be showcasing dishes where fruits are a primary ingredient. Come learn how you can add more fruits into your diet!`,
+      title: 'Fruity Foods',
+      description: `We will be showcasing dishes infused with fruity goodness. Come learn how you can add more fruits to your diet.`,
       start: new Date(y, m+1, 22),
       image: "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
   },
   {
       title: 'Veggie Mix Up',
-      description: `We will be showcasing great dishes that have plenty of veggies and proteins! This is for all those out there who love food, but want to get more vegetables in their diets!`,
+      description: `We will be showcasing great dishes that have plenty of veggies and proteins. This is for all those out there who love food without sacrificing nutrition.`,
       start: new Date(y, m+1, 23),
       image: "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
   },
   {
       title: 'Fry-day',
-      description: `You bring it, we fry it....again. This event is always a huge hit, so we hold it twice!!`,
+      description: `You bring it, we fry it....again. This event is always a huge hit, so we're hosting it twice!`,
       start: new Date(y, m+1, 23),
       image: "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
   },
